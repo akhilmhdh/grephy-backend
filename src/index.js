9ea2cf00817e6b170c5b1f357a7410d7ce37d832
akhilmhdh <akhilmhdh@gmail.com>
2020-04-26 23:19:46 +0530

    =setting express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from 'express'
+
+const app = express()
+
+const port = process.env["PORT"] || 3000;
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(port,()=>{
+    console.log(`Running on Localhost:${port}`)
+})
