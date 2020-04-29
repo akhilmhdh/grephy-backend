13a98e8a5ea96186be6cab9ab43ad9aa1f46b2e7
akhilmhdh <akhilmhdh@gmail.com>
2020-04-29 23:58:55 +0530

    added eslintrc

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
-const env=process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 
-if(env=="development"  || env=="test"){
-    const config=require("./config.json");
-    const envConfig=config[env];
-    Object.keys(envConfig).forEach((key)=>{
-        process.env[key]=envConfig[key];
-    });
-};
+if (env === 'development' || env == 'test') {
+  const config = require('./config.json');
+  const envConfig = config[env];
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import express from 'express'
+import express from 'express';
+import './config/config';
 
-const app = express()
+const app = express();
 
-import "./config/config"
-
-
-app.get('/',(req,res)=>{
-    res.send("hello world");
-})
-
-app.listen(process.env["PORT"],()=>{
-    console.log(`Running on Localhost:${process.env["PORT"]}`)
-})
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Running on Localhost:${process.env.PORT}`);
+});
