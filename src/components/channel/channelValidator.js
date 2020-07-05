700e810f03c9d04785debd35d62e6f2ca459b114
akhilmhdh <akhilmhdh@gmail.com>
2020-07-05 20:39:48 +0530

    channel validator added

--- src/components/channel/channelValidator.js
+++ src/components/channel/channelValidator.js
@@ -7,16 +7,58 @@ import Joi from "@hapi/joi";
 // description: a short note on purpose of this channel.
 // var: these are channel variables for custom variables.
 
-const ChannelSchema = Joi.object({
-  _user: Joi.string().min(1).max(50),
-  name: Joi.string().min(1).max(50).trim(),
-  description: Joi.string().min(1).max(150).trim(),
-  var: Joi.array().items(
-    Joi.object({
-      name: Joi.string().min(1).max(30).required(),
-      value: Joi.any(),
-    })
-  ),
+// const ChannelSchema = Joi.object({
+//   _user: Joi.string().min(1).max(50),
+//   name: Joi.string().min(1).max(50).trim(),
+//   description: Joi.string().min(1).max(150).trim(),
+//   var: Joi.array().items(
+//     Joi.object({
+//       name: Joi.string().min(1).max(30).required(),
+//       value: Joi.any(),
+//     })
+//   ),
+// });
+
+const createChannel = Joi.object({
+  _user: Joi.string().min(1).max(50).required(),
+  name: Joi.string().min(1).max(50).trim().required(),
+  description: Joi.string().min(1).max(150).trim().required(),
 });
 
-export default ChannelSchema;
+const updateChannel = Joi.object({
+  _id: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+  _user: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required()
+    .required("Unauthorized"),
+  name: Joi.string().min(1).max(50).trim(),
+  description: Joi.string().min(1).max(150).trim(),
+});
+
+const readChannel = Joi.object({
+  _id: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+});
+
+const listChannel = Joi.object({
+  _user: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+});
+
+const deleteChannel = Joi.object({
+  _id: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required(),
+});
+
+export default {
+  createChannel,
+  updateChannel,
+  listChannel,
+  readChannel,
+  deleteChannel,
+};
